b36efa1806dda40606a6139defd3a89dc18e3daa
reincurioso <reniellecurioso@gmail.com>
2015-12-08 13:39:23 +0800

    fix font / hightlight

--- SourceCode/FilPortTrackingSystem/application/views/footer/legend.php
+++ SourceCode/FilPortTrackingSystem/application/views/footer/legend.php
@@ -9,8 +9,13 @@
 						<table style="font-size: 90%;">
 							<?php
 								foreach ($status as $row) {
-							 ?>
-							<tr><td  colspan="4" style="background-color: <?php echo $row->ColorCode; ?>;"><?php echo $row->StatusName; ?></td>
+								$status = $row->StatusName;
+								   if($status=='Red Font'){	?>
+							<tr>
+							<td  colspan="4" style="color: <?php echo $row->ColorCode; ?>;"><?php echo $row->StatusName; ?></td>
+							<?php }else{?>
+							<td  colspan="4" style="background-color: <?php echo $row->ColorCode; ?>;"><?php echo $row->StatusName; ?></td>
+							<?php }?>
 								<td style="padding-left:5px; text-align: left;"><?php echo $row->Description; ?></td>
 							</tr>
 							<?php } ?>
