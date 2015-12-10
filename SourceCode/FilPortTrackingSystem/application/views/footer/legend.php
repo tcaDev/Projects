66fb8427ad6a7febcb6f145eb5beecff7911ad56
reincurioso <reniellecurioso@gmail.com>
2015-12-10 15:14:21 +0800

    updated

--- SourceCode/FilPortTrackingSystem/application/views/footer/legend.php
+++ SourceCode/FilPortTrackingSystem/application/views/footer/legend.php
@@ -6,15 +6,14 @@
 						<table style="font-size: 83%;">
 							<?php
 								foreach ($status as $row) {
-								$status = $row->StatusName;
-								   if($status=='Red Font'){	?>
+								$status = $row->IsBackground;
+								   if($status==0){	?>
 							<tr>
 							<td  colspan="4" style="color: <?php echo $row->ColorCode; ?>;border: 2px solid #fff;"><?php echo $row->StatusName; ?></td>
 							<?php }else{?>
 							<td  colspan="4" style="background-color: <?php echo $row->ColorCode; ?>;border: 2px solid #fff;"><?php echo $row->StatusName; ?></td>
 							<?php }?>
 								<td style="padding-left:5px; text-align: left;border: 1px solid #fff;"><?php echo $row->Description; ?></td>
-
 							</tr>
 							<?php } ?>
 						</table>
