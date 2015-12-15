1256f3a5e29d8ac4f1b3d7981f984ad1eee77613
reincurioso <reniellecurioso@gmail.com>
2015-12-15 11:29:22 +0800

    added filter sidebar

--- SourceCode/FilPortTrackingSystem/application/views/global/global_page.php
+++ SourceCode/FilPortTrackingSystem/application/views/global/global_page.php
@@ -1,53 +1,58 @@
+	<div class="gl-filter">
+		<div class="gl-filtitle">
+			<h1>Filters</h1>
+		</div>
+
+	<h4>Type:</h4>
+		<select class="form-control">
+			<option>Jobfile</option>
+			<option>Shipper</option>
+			<option>Broker</option>
+		</select><br>
+
+	<h4>Limit:</h4>	
+<!-- 			<?php //for ($x = 1; $x <= 10; $x++) {
+			?> -->
+		<select class="form-control">
+<!-- 				<?php// echo "<option> $x </option>"; }?> -->
+				<option>1</option>
+				<option>2</option>
+		</select><br>
+
+	<h4>Date Created:</h4>
+		<h6>From:</h6>
+		<input class="form-control" type="date"> 
+		<h6>To:</h6>
+		<input class="form-control" type="date">
+	</div>
 
 	<div class="gl-bar">
 
-						<div class="input-group col-lg-12">
+		<div class="input-group col-lg-12">
 
-			            <input type="text" class="  search-query form-control" placeholder="Search" onkeyup="search(this.value)" />
+            <input type="text" class="  search-query form-control" placeholder="Search" onkeyup="search(this.value)" />
 
-			            <span class="input-group-btn">
+            <span class="input-group-btn">
 
-			                <button class="btn" type="submit">
+                <button class="btn" type="submit">
 
-			                    <span class=" glyphicon glyphicon-search"></span>
+                    <span class=" glyphicon glyphicon-search"></span>
 
-			                </button>
+                </button>
 
-			            </span>
+            </span>
 
-			        </div>
+        </div>
 
-		        	<p><a class="btn pull-left" data-toggle="collapse" data-target="#viewdetails">
-
-					<span color:red;>Search filter &raquo;</span></a></p>
-
-					<p class="collapse pull-left" id="viewdetails">
-
-						<label class="checkbox-inline"><input type="checkbox" value="">Jobfile</label>
-
-						<label class="checkbox-inline"><input type="checkbox" value="">Container no.</label>
-
-						<label class="checkbox-inline"><input type="checkbox" value="">Consignee</label>
-
-					</p>
+    	<p><a class="btn pull-left" data-toggle="collapse" data-target="#viewdetails">
 
 	</div>
-
 		<div class="gl-container">
-
 		 <span id="result"></span>
-
 		</div>
-
 	</div>
-
 </body>
-
 </html>
-
-
-
-
 
 <script>
 
