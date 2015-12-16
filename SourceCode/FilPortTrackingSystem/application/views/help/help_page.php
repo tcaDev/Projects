e0b528581f569d81e528f46dae7b540eefe6c257
reincurioso <reniellecurioso@gmail.com>
2015-12-16 18:06:04 +0800

    change link

--- SourceCode/FilPortTrackingSystem/application/views/help/help_page.php
+++ SourceCode/FilPortTrackingSystem/application/views/help/help_page.php
@@ -1,7 +1,7 @@
 
 
 		<div class="help-container">
-			<iframe src="http://docs.google.com/gview?url=http://filporttracking.esy.es/filport/resources/img/Meetingnotes.pdf&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>
+			<iframe src="http://docs.google.com/gview?url=http://topconnection.asia/filport/resources/img/Meetingnotes.pdf&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>
 		</div>
 	</div>
 </body>
