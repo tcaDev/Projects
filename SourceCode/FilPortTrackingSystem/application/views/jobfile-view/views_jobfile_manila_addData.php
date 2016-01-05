a618aa4915372b6740d2266b846b8f954ec29d88
Reinen <reinenconstantino@gmail.com>
2016-01-05 14:43:03 +0800

    Reload Page when Click cancel in Add Form

--- SourceCode/FilPortTrackingSystem/application/views/jobfile-view/views_jobfile_manila_addData.php
+++ SourceCode/FilPortTrackingSystem/application/views/jobfile-view/views_jobfile_manila_addData.php
@@ -622,7 +622,7 @@
  <!--     <button type="button" class="btn btn-danger container_insert">test insert</button> -->
       <button type="button" class="btn btn-danger btn-Next submit_jobfile" >Next</button>
       <button type="button" class="hidden btn btn-danger test_data save_charge" >Save</button>
-      <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+      <button type="button" class="btn btn-danger btn-cancel-mnla" data-dismiss="modal">Cancel</button>
     </div>
   </div>
 
@@ -644,6 +644,10 @@
 
   <script>
 	$(document).ready(function(){
+
+		$(".btn-cancel-mnla").click(function(){
+			location.reload();
+		});
 
     $('[data-toggle="tooltip"]').tooltip(); 
 
