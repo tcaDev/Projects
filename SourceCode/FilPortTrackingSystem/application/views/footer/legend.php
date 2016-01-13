52448ffa6bfa24e166555c9316f37d4e0fc88b87
danieltenefrancia <daniel.tenefrancia@gmail.com>
2016-01-13 11:40:23 +0800

    Resize Legend Tab

--- SourceCode/FilPortTrackingSystem/application/views/footer/legend.php
+++ SourceCode/FilPortTrackingSystem/application/views/footer/legend.php
@@ -1,10 +1,9 @@
   <fieldset >
-    <legend align="left">Legend For Color Stages:</legend>
-			<div class="legend-body-font" style="overflow-x:auto;font-size: 100%;line-height: 1.6875;">
+    <legend align="left" style="font-size:12px;">Legend For Color Stages:</legend>
+			<div class="legend-body-font" style="overflow-x:auto;font-size:12px;line-height: 1.6875;">
 				<div class="container-fluid">
 					<div class="row">
 						<div class="col-lg-12 col-md-12 col-sm-12">
-								
 							<?php
 							foreach ($status as $row) {
 							$status = $row->IsBackground;
@@ -13,19 +12,18 @@
 							    	<div class="col-lg-1 col-md-1 col-sm-1" style="color:<?php echo $row->ColorCode; ?>;">
 										<span><?php echo $row->StatusName; ?></span>
 									</div>
-									<div class="col-lg-5 col-md-5 col-sm-5 " >
-										<p><?php echo $row->Description; ?></p>
+									<div class="col-lg-3 col-md-3 col-sm-5 " >
+										<?php echo $row->Description; ?>
 									</div>
 							    </div>
-									
 									<?php }else{?>
 								<div>
 									<div class="col-lg-1 col-md-1 col-sm-1" style="background-color: <?php echo $row->ColorCode; ?>;">
 										<span><?php echo $row->StatusName; ?></span>
 									</div>
 
-									<div class="col-lg-5 col-md-5 col-sm-5 " >
-										<p><?php echo $row->Description; ?></p>
+									<div class="col-lg-3 col-md-3 col-sm-5 " >
+										<?php echo $row->Description; ?>
 									</div>	
 								</div>
 																	
@@ -38,4 +36,7 @@
 				</div>
 			</div>
   </fieldset>
+  <style>
+
+  </style>
 
