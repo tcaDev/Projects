ae0f4edee97c033ec9895249039390a64482d4ea
doublesabog <eli_montefalcon@yahoo.com>
2016-01-25 23:45:26 -0800

    menu for consignee

---
+++ SourceCode/FilPortTrackingSystem/application/views/menu/views_menu_consignee.php
@@ -0,0 +1,148 @@
+
+		<div class="container-fluid">
+			<div class="menu-container">
+				<div class="row">
+						<!-- Global Search -->
+					<a style="display:block" href="<?php echo base_url('Login_user/reports')?>">
+					<div class="menu-tiles2 effect__hover" >
+						<div class="col-lg-4 col-md-4 col-sm-4  btn btn-report card__front" style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<span class="fa fa-line-chart img-jobfile"></span>
+								</div>
+
+								<div class="col-lg-8 col-md-8 col-sm-8">
+									<span class="report">Reports</span>
+								</div>
+								
+							</div>							
+						</div>
+
+						<div class="col-lg-4 col-md-4 col-sm-4 btn btn-report card__back" style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<span class="fa fa-pencil-square-o img-jobfile"></span>
+								</div>
+								<div class="col-lg-8 col-md-8 col-sm-8 pull-right">
+									<span class="job-file pull-right">Consignee<br>Transaction</span>
+								</div>
+							</div>		
+						</div>
+					</div>
+					</a>
+
+					<a style="display:block" href="<?php echo base_url('Login_user/search')?>">
+					<div class="menu-tiles2 effect__hover">
+						<div class="col-lg-4 col-md-4 col-sm-4  btn btn-gbl card__front " style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<span class="fa fa-globe img-jobfile"></span>
+								</div>
+
+								<div class="col-lg-8 col-md-8 col-sm-8">
+									<span class="gbl-srch">Global<br> Search</span>
+								</div>
+								
+							</div>							
+						</div>
+
+						<div class="col-lg-4 col-md-4 col-sm-4 btn btn-gbl card__back" style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<span class="fa fa-search img-jobfile"></span>
+								</div>
+								<div class="col-lg-8 col-md-8 col-sm-8 pull-right">
+									<span class="job-file pull-right">Quick Jobfile<br>View</span>
+								</div>
+							</div>		
+						</div>
+					</div>
+					</a>
+					</div>
+
+				<div class="row">
+
+						<!-- Report -->
+				
+						<!-- Dashboard -->	
+
+					<!-- 	<a style="display:block" href="<?php //echo base_url('Login_user/dashboard')?>"> -->
+					<div class="menu-tiles2 effect__hover">
+						<div class="col-lg-4 col-md-4 col-sm-4 btn btn-dashboard card__front imgDisabled" style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<span class="fa fa-pie-chart img-jobfile"></span>
+								</div>
+
+								<div class="col-lg-8 col-md-8 col-sm-8">
+									<span class="dshbrd">Dashboard</span>
+								</div>
+								
+							</div>							
+						</div>
+
+						<div class="col-lg-4 col-md-4 col-sm-4 btn btn-dashboard card__back imgDisabled" style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<span class="fa fa-dashboard img-jobfile"></span>
+								</div>
+								<div class="col-lg-8 col-md-8 col-sm-8 pull-right">
+									<span class="job-file pull-right">Management<br>Metrics</span>
+								</div>
+							</div>							
+						</div>
+					</div>
+					</a>
+
+						<!-- Help -->
+						<!-- <a style="display:block" href="<?php //echo base_url('Login_user/help')?>"> -->
+					<div class="menu-tiles2 effect__hover">
+						<div class="col-lg-4 col-md-4 col-sm-4 btn btn-help card__front imgDisabled" style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4 imgDisabled">
+									<span class="fa fa-question img-jobfile"></span>
+								</div>
+
+								<div class="col-lg-8 col-md-8 col-sm-8">
+									<span class="help">Help</span>
+								</div>
+								
+							</div>							
+						</div>
+
+						<div class="col-lg-4 col-md-4 col-sm-4 btn btn-help card__back imgDisabled" style="box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);">
+							<div class="row">
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<span class="fa fa-book img-jobfile"></span>
+								</div>
+								<div class="col-lg-8 col-md-8 col-sm-8 pull-right">
+									<span class="job-file pull-right">Online User<br>Manual</span>
+								</div>
+								
+							</div>							
+						</div>
+					</div>
+					</a>
+				</div>
+				<br>
+				<br>
+				<hr class="line">
+			</div>	
+			</div>
+
+
+	<!-- Footer -->
+		<div class="col-lg-12 col-md-12 col-sm-12 footer hidden">
+			<?php $this->load->view('footer/date-time'); ?>
+		</div>
+	<!-- End Footer -->
+	</div>
+</body>
+<script type="text/javascript">
+	$(".imgDisabled").on("click",function(){
+	alert('Sorry for the Inconvenience!. This Page is currently under Construction');
+});
+</script>
+</html>
+
+
