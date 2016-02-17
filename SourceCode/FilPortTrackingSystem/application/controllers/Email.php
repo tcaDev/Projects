a7d16680a6f3f8b3e83575b2d4e522d84ca98a67
doublesabog <eli_montefalcon@yahoo.com>
2016-02-17 15:32:55 +0800

    email test3

--- SourceCode/FilPortTrackingSystem/application/controllers/Email.php
+++ SourceCode/FilPortTrackingSystem/application/controllers/Email.php
@@ -462,7 +462,13 @@ $now = $date_now[0];
 */
         $i++;
         echo  '
-        <html>
+        <html >
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="text/html">
+
         
         <body>
         <div class="change_print'.$i.'"  > 
