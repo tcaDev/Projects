ea9556d46ae52eabd52073c448ecaca6dcfd0fb9
danieltenefrancia <daniel.tenefrancia@gmail.com>
2016-03-10 20:54:43 +0800

    Reports - Consignee - Fix Size Modal

--- SourceCode/FilPortTrackingSystem/application/views/reports/reports_page_consignee.php
+++ SourceCode/FilPortTrackingSystem/application/views/reports/reports_page_consignee.php
@@ -234,6 +234,7 @@
 					  		<br>
 					  		<h4 style="padding-left: 25px;">Running Charges</h4>
 					  		<br>
+					  		<div style="height: 300px; overflow-y: auto;">
 						       <table id="tbl-third-report-data" class="table table-striped table-bordered table-layout:fixed" style="cursor:pointer;width:100%;text-align:left;">
 						       		<tr>
 						             	<td>
@@ -241,6 +242,7 @@
 						             	</td>
 						            </tr>
 						       </table>
+						     </div>
 				    </div>
 
 					<div id="status-reports" class="col-lg-12 col-md-12 col-sm-12 table-content tab-pane fade tab-status tab-clear">
@@ -248,6 +250,7 @@
 					  		<h4 style="padding-left: 25px;">Status Report</h4>
 					  		<h6> <i> *Double Click the Status Report to View the Full Report.</i></h6>
 					  		<br>
+					  		<div style="height: 300px; overflow-y: auto;">
 						      <table id="tbl-fourth-report-data" class="table table-striped tableOverFlow" style="cursor:pointer;">
 						             	<tr>
 						             		<td class="loadReports tdOverFlow" style="width:100%;" id="loadReports">
@@ -255,13 +258,14 @@
 						             		</td>
 						             	</tr>
 						      </table>
+						    </div>
 					</div>
 
 					<div id="other-documents" class="col-lg-12 col-md-12 col-sm-12 table-content tab-pane fade in active tab-otherdocs tab-clear">
-		 				<div style="height: 300px; overflow-y: auto;">
 		 					<br>
 					  		<h4 style="padding-left: 25px;">Other Documents</h4>
 					  		<br>
+		 					<div style="height: 300px; overflow-y: auto;">
 						        <?php 
 								$element = "
 									<div class='list-group gallery'>
