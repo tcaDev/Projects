e8c5126749c6eca78d93036dd698feed03be836f
doublesabog <eli_montefalcon@yahoo.com>
2016-03-28 14:53:56 +0800

    legend insert data

--- SourceCode/FilPortTrackingSystem/application/models/User.php
+++ SourceCode/FilPortTrackingSystem/application/models/User.php
@@ -66,7 +66,7 @@ Class User extends CI_Model
   }
 
 
-  function update_cosignee($id,$name,$hbno,$vilage,$city,$country,$ofnum,$status){
+  function update_cosignee($id,$name,$hbno,$vilage,$city,$country,$ofnum,$status,$userid){
      $status =  (int) $status;
 
    /*  $query = $this->db->query('Select * from Countries where CountryName=$country');
@@ -82,18 +82,35 @@ Class User extends CI_Model
         'TownOrCityProvince'  =>$city,
         'CountryId'           =>$country,
         'OfficeNumber'        => $ofnum,
-        'DateAdded'           => date('Y-m-d'),
+/*        'DateAdded'           => date('Y-m-d'),*/
         'IsActive'            => $status
        /* 'DateAdded' => $date*/
 
         );
           $this->db->where('ConsigneeId', $id);
           $this->db->update('Consignee', $data);
+
+         //get the ConsigneeId based on consignee name
+        $data_history = array(
+        'ConsigneeId'         => $id,
+        'ConsigneeName'       => $name,
+        'HouseBuildingNoOrStreet' => $hbno,
+        'BarangayOrVillage'   =>$vilage,
+        'TownOrCityProvince'  =>$city,
+        'CountryId'           =>$country,
+        'OfficeNumber'        => $ofnum,
+/*        'DateAdded'           => date('Y-m-d'),*/
+        'IsActive'            => $status,
+        'DateUpdated'         => date('Y-m-d'),
+        'UpdatedBy_UserId'    => $userid
+
+        );
+         $this->db->insert('ConsigneeHistory', $data_history);
     
       
 
   }
-    function update_vessel($id,$name,$address_carrier,$number_carrier,$status){
+    function update_vessel($id,$name,$address_carrier,$number_carrier,$status,$userid){
         $status =  (int) $status;
         $data = array(
         'CarrierName'    => $name,
@@ -104,15 +121,26 @@ Class User extends CI_Model
         );
           $this->db->where('CarrierId', $id);
           $this->db->update('Carrier', $data);
+
+        $data_history = array(
+          'CarrierId'      => $id,
+          'CarrierName'    => $name,
+          'Address'        => $address_carrier,
+          'OfficeNo'       => $number_carrier,
+          'IsActive'       => $status,
+          'DateUpdated'    => date('Y-m-d'),
+          'UpdatedBy_UserId'   => $userid
+        );
+        $this->db->insert('CarrierHistory',$data_history);
   }
 
      /* function update_shipper($id,$name,$fname,$mname,$lname,$c1,$c2){
       */
-       function update_shipper($id,$name,$hbno,$vilage,$city,$country,$status){
+       function update_shipper($id,$name,$hbno,$vilage,$city,$country,$status,$userid){
          $status =  (int) $status;
         $data = array(
         'ShipperName'               => $name,
-        'DateAdded'                 => date('Y-m-d'),
+ /*       'DateAdded'                 => date('Y-m-d'),*/
         'HouseBuildingNoStreet'     => $hbno,
         'BarangarOrVillage'         => $vilage,
         'TownOrCityProvince'        => $city,
@@ -121,6 +149,20 @@ Class User extends CI_Model
         );
           $this->db->where('ShipperId', $id);
           $this->db->update('Shipper', $data);
+
+          $data_history = array(
+            'ShipperId'                 => $id,
+            'ShipperName'               => $name,
+/*            'DateAdded'                 => date('Y-m-d'),*/
+            'HouseBuildingNoStreet'     => $hbno,
+            'BarangarOrVillage'         => $vilage,
+            'TownOrCityProvince'        => $city,
+            'CountryId'                 => $country,
+            'IsActive'                  => $status,
+            'DateUpdated'               => date('Y-m-d'),
+            'UpdatedBy_UserId'          => $userid
+        );
+         $this->db->insert('ShipperHistory',$data_history);
 
 /*        $data2 = array(
                 'FirstName'        =>$fname,
@@ -139,7 +181,7 @@ Class User extends CI_Model
 
 
 // FOR UPDATING CONTACT PERSONS IN SETTINGS START
-    function update_shippercon($id,$fname,$mname,$lname,$c1,$c2){
+    function update_shippercon($id,$fname,$mname,$lname,$c1,$c2,$userid){
 
               $data2 = array(
                 'FirstName'        =>$fname,
@@ -152,8 +194,21 @@ Class User extends CI_Model
           );
          $this->db->where('ShipperContactId', $id);
          $this->db->update('ShipperContacts', $data2);
+
+
+          $data_history = array(
+                'ShipperContactId' =>$id,
+                'FirstName'        =>$fname,
+                'MiddleName'       =>$mname,
+                'LastName'         =>$lname,
+                'ContactNo1'       =>$c1,
+                'ContactNo2'       =>$c2,
+                'DateUpdated'      => date('Y-m-d'),
+                'UpdatedBy_UserId' => $userid
+          );
+           $this->db->insert('ShipperContactsHistory',$data_history);
     }
-    function update_consigneecon($id,$fname,$mname,$lname,$c1,$c2){
+    function update_consigneecon($id,$fname,$mname,$lname,$c1,$c2,$userid){
 
               $data2 = array(
                 'FirstName'        =>$fname,
@@ -161,17 +216,27 @@ Class User extends CI_Model
                 'LastName'         =>$lname,
                 'ContactNo1'       =>$c1,
                 'ContactNo2'       =>$c2
-
-
           );
          $this->db->where('ConsigneeContactId', $id);
          $this->db->update('ConsigneeContacts', $data2);
+
+        $data_history= array(
+                'ConsigneeContactId'=>$id,
+                'FirstName'        =>$fname,
+                'MiddleName'       =>$mname,
+                'LastName'         =>$lname,
+                'ContactNo1'       =>$c1,
+                'ContactNo2'       =>$c2,
+                'DateUpdated'      => date('Y-m-d'),
+                'UpdatedBy_UserId' => $userid
+          );
+         $this->db->insert('ConsigneeContactsHistory',$data_history);
     }
 // FOR UPDATING CONTACT PERSONS IN SETTINGS END
 
         function update_broker($id,$broker_fname,$broker_mname,
       $broker_lname,$broker_houseno,$broker_vil,$broker_city,
-      $broker_cid,$broker_contact1,$broker_contact2,$status_broker){
+      $broker_cid,$broker_contact1,$broker_contact2,$status_broker,$userid){
 
         $data = array(
         'FirstName'         => $broker_fname,
@@ -184,14 +249,30 @@ Class User extends CI_Model
         'TownOrCityProvince'=> $broker_city,
         'CountryId'         => $broker_cid,
         'IsActive'          => $status_broker
-
         );
           $this->db->where('BrokerId', $id);
           $this->db->update('Broker', $data);
+
+      $data_history = array(
+            'BrokerId'          => $id,
+            'FirstName'         => $broker_fname,
+            'MiddleName'        => $broker_mname,
+            'LastName'          => $broker_lname,
+            'ContactNo1'        => $broker_contact1,
+            'ContactNo2'        => $broker_contact2,
+            'HouseBuildingNoStreet' =>$broker_houseno,
+            'BarangarOrVillage' => $broker_vil,
+            'TownOrCityProvince'=> $broker_city,
+            'CountryId'         => $broker_cid,
+            'IsActive'          => $status_broker,
+            'DateUpdated'      => date('Y-m-d'),
+            'UpdatedBy_UserId' => $userid
+        );
+       $this->db->insert('BrokerHistory',$data_history);
     
   }
 
-    function update_products($id,$name,$status){
+    function update_products($id,$name,$status,$userid){
         $data = array(
         'ProductName'    => $name,
         'IsActive'       => $status
@@ -199,6 +280,14 @@ Class User extends CI_Model
         );
           $this->db->where('ProductId', $id);
           $this->db->update('Products', $data);
+
+        $data_history = array(
+        'ProductId'      => $id,
+        'ProductName'    => $name,
+        'IsActive'       => $status,
+        'DateUpdated'      => date('Y-m-d'),
+        'UpdatedBy_UserId' => $userid
+        );
     
   }
 
@@ -325,6 +414,40 @@ Class User extends CI_Model
     $count = $this -> db -> get();
     return $count->result();
     }
+
+
+
+//for getting ids in settings start
+function get_consigneeid($consignee){
+    $this ->db->select('*');
+    $this ->db->from('Consignee');
+    $this ->db->where('ConsigneeId');
+    $count = $this -> db -> get();
+    return $count->result();
+}
+
+function get_brokerid($fname,$mname,$lname){
+    $this ->db->select('*');
+    $this ->db->from('Broker');
+    $this ->db->where('FirstName',$fname);
+    $this ->db->where('MiddleName',$mname);
+    $this ->db->where('LastName',$lname);
+    $count = $this -> db -> get();
+    return $count->result();
+}
+
+function get_carrierid($carrier){
+    $this ->db->select('*');
+    $this ->db->from('Carrier');
+    $this ->db->where('CarrierId',$carrier);
+    $count = $this -> db -> get();
+    return $count->result();
+}
+
+
+
+//for getting ids in settings end
+
 
 
 
@@ -694,6 +817,17 @@ function get_jobfile_outport(){
 
           $this->db->where('StatusId', $legend_id);
           $this->db->update('Status', $data); 
+
+         $data_history = array(
+                'StatusId'   => $legend_id,
+                'StatusName' => $legend_status,
+                'Description'=> $description,
+                'ColorCode'  => $legend_color,
+                'IsActive'   => $status,
+                'IsBackground' => $back_ground
+          );
+
+          $this->db->insert('StatusHistory',$data_history);
   }
     function update_container($container_id,$con,$con_descrip,$con_size){
          $data = array(
@@ -792,8 +926,20 @@ function get_jobfile_outport(){
     $this->db->select('*');
     $this->db->from('vw_JobFile');
     $this->db->where('MonitoringTypeId',$montype);
- /*   $this->db->order_by('JobFileId', 'desc');*/
     $this->db->like('JobFileNo',$jobfile);
+    $this->db->order_by('JobFileId', 'desc');
+    $this->db->limit($page_position,$item_per_page); 
+    $query = $this->db->get();
+      return $query->result();
+  }
+
+    function findlimit_search_air($page_position,$item_per_page){
+    //$this->db->cache_on();
+    $this->db->select('*');
+    $this->db->from('vw_JobFile');
+    $this->db->where('MonitoringTypeId',$montype);
+    $this->db->like('JobFileNo',$jobfile);
+    $this->db->order_by('JobFileId', 'desc');
     $this->db->limit($page_position,$item_per_page); 
     $query = $this->db->get();
       return $query->result();
@@ -848,16 +994,13 @@ function get_jobfile_outport(){
   /* $query = $this->db->query("select * from vw_JobFile order by JobFileId desc ",$page_position, $item_per_page);*/
   }
 
-    function findlimit_search_air($page_position,$item_per_page,$jobfile){
-     // $this->db->cache_on();
+
+    function mm(){
     $this->db->select('*');
-    $this->db->from('vw_JobFileAir');
-    $this->db->like('JobFileNo',$jobfile);
-    $this->db->limit($page_position,$item_per_page); 
+    $this->db->from('vw_JobFile');
     $query = $this->db->get();
       return $query->result();
   }
-
 
 
 
