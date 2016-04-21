c535a837619166eabc09a3f9d5ef2c62960fdd2a
Reinen <reinenconstantino@gmail.com>
2016-04-21 18:32:14 +0800

    fixed bugs Air Freight Truck booking

--- SourceCode/FilPortTrackingSystem/application/controllers/Print_Report_Truck_Admin.php
+++ SourceCode/FilPortTrackingSystem/application/controllers/Print_Report_Truck_Admin.php
@@ -116,76 +116,88 @@
   function loadReportTruck($monitoringType , $cID , $ataFrom , $ataTo){
     $report_truck = $this->Charges->getCharges_Truck($monitoringType , $cID , $ataFrom , $ataTo); 
   
-    if($monitoringType == 1 || $monitoringType == 2){
+    if($monitoringType == 1|| $monitoringType == 2){
       if(count($report_truck) > 0){
-         $dispOutput .="<br><table class ='table table-condensed table-bordered '>
-                    <thead >
+         $dispOutput .="<table class ='table table-bordered'>
+                    <thead>
                     <tr>
                       <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Target Delivery Date</th>
                       <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Jobfile No</th>
                       <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Container No</th>
                       <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Commodity</th>
-                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Origin</th>
-                    ";
-                    if($monitoringType == 1){
-                      $dispOutput .=" <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Gate In (Date/Time)</th>
-                                    <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Gate Out (Date/Time)</th>";
-                    }
-          
-                     $dispOutput .="<th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Actual Delivery Date</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Origin</th>";
+                      if($monitoringType == 1){
+                         $dispOutput .="  
+                         <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Gate In (Date/Time)</th>
+                         <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Gate Out (Date/Time)</th>";
+                      }
+                    
+         $dispOutput .=" <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Actual Date Delivered</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Start Of Storage</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Total Storage</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Start Of Demurrage</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Total Demurrage</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Total Running Charges</th>
                       <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Shipping Line</th>
-                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Trucking Company</th>
                     </tr>
                     </thead>
                     <tbody>
                     "; 
           foreach ($report_truck as $row){
                $dispOutput .= "<tr>
-                                <td style='padding:5px;'>" . $row->TargetDeliveryDate . "</td>
-                                <td style='padding:5px;'>" . $row->JobFileNo . "</td>
-                                <td style='padding:5px;'>" . $row->ContainerNo . "</td>
-                                <td style='padding:5px;'>" . stripslashes($row->ProductName) . "</td>
-                                <td style='padding:5px; '>" . stripslashes($row->Origin) . "</td> ";
-
-                    if($monitoringType == 1){
-                      $dispOutput .=" <td style='padding: 5px;'>" . $row->GateInAtPort . "</td>
-                                <td style='padding:5px;'>" . $row->GateOutAtPort . "</td>";
-                    }
-                $dispOutput .= "
-                                <td style='padding:5px;'>" . $row->ActualDeliveryAtWarehouse . "</td>
-                                <td style='padding:5px;'>" . $row->CarrierName . "</td>
-                                <td style='padding:5px;'>" . $row->HaulerOrTruck . "</td>
+                                <td style='padding:10;' >" . $row->TargetDeliveryDate . "</td>
+                                <td style='padding:10px;' >" . $row->JobFileNo . "</td>
+                                <td style='padding:10px;' >" . $row->ContainerNo . "</td>
+                                <td style='padding:10px;' >" . stripslashes($row->ProductName) . "</td>
+                                <td style='padding:10px;' >" . stripslashes($row->Origin) . "</td>";
+                  if($monitoringType == 1){
+               $dispOutput .= " <td style='padding:10px;' >" . $row->GateInAtPort . "</td>
+                                <td style='padding:10px;' >" . $row->GateOutAtPort . ")</td>";
+                  }
+                               
+              $dispOutput .= " <td style='padding:10px;' >" . $row->ActualDeliveryAtWarehouse . "</td>
+                                <td style='padding:10px;'>" . $row->StartOfStorage . "</td>
+                                <td style='padding:10px;' >" . number_format($row->Storage,2,'.',',') . "</td>
+                                <td style='padding:10px;'>" . $row->StartOfDemorage . "</td>
+                                <td style='padding:10px;'>" . number_format($row->Demorage,2,'.',','). "</td>
+                                <td style='padding:10px;'>" . number_format($row->Total_Charges,2,'.',',') . "</td>
+                                <td style='padding:10px;'>" . $row->CarrierName . "</td>
                               </tr>";
           }
           $dispOutput .= "</tbody>";
         }else{
-               $dispOutput = "<center style='font-color:Red;font-size:18px;'>No Records of Truck with this Span of Target Delivery Date </center>";      
+               $dispOutput = "<center style='font-color:Red;font-size:18px;'> No Records of Truck witd tdis Span of Target Delivery Date </center>";      
         }
       }else{
         if(count($report_truck) > 0){
-         $dispOutput .="<table class ='table table-condensed table-bordered'>
-                    <thead >
-                    <tr >
-                      <th style='text-align:center;background-color:#ccc;padding:5px;'>Target Delivery Date</th>
-                      <th  style='text-align:center;background-color:#ccc;padding:5px;'>Jobfile No</th>
-                      <th  style='text-align:center;background-color:#ccc;padding:5px;'>No. of Cartons</th>
-                      <th  style='text-align:center;background-color:#ccc;padding:5px;'>Gross Weight</th>
-                      <th  style='text-align:center;background-color:#ccc;padding:5px;'>Commodity</th>
-                      <th  style='text-align:center;background-color:#ccc;padding:5px;'>Origin</th>
-                      <th  style='text-align:center;background-color:#ccc;padding:5px;'>Airline/Forwarder</th>
+         $dispOutput .="<table class ='table table-bordered'>
+                    <tdead>
+                    <tr>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Target Delivery Date</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Jobfile No</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Number Of Cartons</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Gross Weight</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Commodity</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Origin</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Total Storage</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Total Running Charges</th>
+                      <th style='text-align:center;background-color:#ccc; border:1px solid #ddd;'>Airline/Forwarder</th>
                     </tr>
                     </thead>
                     <tbody>
                     "; 
           foreach ($report_truck as $row){
+              $volume = $this->Charges->getVolume($monitoringType, $row->JobFileNo);
                $dispOutput .= "<tr>
-                                <td style='padding-left:10px;>" . $row->TargetDeliveryDate . "</td>
-                                <td style='padding-left:10px;>" . $row->JobFileNo . "</td>
-                                <td style='padding-left:10px;>" . $row->NoOfCartons . "</td>
-                                <td style='padding-left:10px;>" . $row->GrossWeight . "</td>
-                                <td style='padding-left:10px;>" . stripslashes($row->ProductName) . "</td>
-                                <td style='padding-left:10px;>" . stripslashes($row->Origin) . "</td>
-                                <td style='padding-left:10px;>" . $row->Forwarder . "</td>
+                                <td style='padding:10px;' >" . $row->TargetDeliveryDate . "</td>
+                                <td style='padding:10px;'>" . $row->JobFileNo . "</td>
+                                <td style='padding:10px;'>" . $row->NoOfCartons . "</td>
+                                <td style='padding:10px;'>" . $row->GrossWeight . "</td>
+                                <td style='padding:10px;'>" . stripslashes($row->ProductName) . "</td>
+                                <td style='padding:10px;'>" . stripslashes($row->Origin) . "</td>
+                                <td style='padding:10px;'>" . number_format($row->StorageFee,2, '.' , ',') . "</td>
+                                <td style='padding:10px;'>" . number_format($row->Total_Charges,2, '.' , ',') . "</td>
+                                <td style='padding:10px;'>" . $row->Forwarder . "</td>
                               </tr>";
           }
           $dispOutput .= "</tbody>";
@@ -193,6 +205,7 @@
                $dispOutput = "<center style='font-color:Red;font-size:18px;'> No Records of Truck with this Span of Target Delivery Date </center>";      
         }
       }
+
       return $dispOutput;
   }
 }
